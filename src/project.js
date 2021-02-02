ac58c8d9b8e1d4eb85666d9b74990eb38ff0418c
liuchang <451519704@qq.com>
2021-02-02 20:30:13 +0800

    修改弹性系数

--- src/project.js
+++ src/project.js
@@ -1164,7 +1164,7 @@ window.__require = function e(t, n, o) {
                         n.scale = 0,
                         n.getComponent(cc.RigidBody).type = cc.RigidBodyType.Static,
                         n.getComponent(cc.PhysicsCircleCollider).radius = 0,
-                        n.getComponent(cc.PhysicsCircleCollider).reduction = 0.2,
+                        n.getComponent(cc.PhysicsCircleCollider).restitution = 0.9,
                         n.getComponent(cc.PhysicsCircleCollider).apply(),
                         cc.tween(n).to(.5, {
                         scale: 1
